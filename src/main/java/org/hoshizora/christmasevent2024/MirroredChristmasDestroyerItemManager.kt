1b2fbbeee817b91e20e332704eab27cc13116e72
HoshizoraRenaru <hoshizorarenaru@gmail.com>
2024-11-28 00:36:57 +0900

    Little fix

--- src/main/java/org/hoshizora/christmasevent2024/MirroredChristmasDestroyerItemManager.kt
+++ src/main/java/org/hoshizora/christmasevent2024/MirroredChristmasDestroyerItemManager.kt
@@ -18,8 +18,8 @@ object MirroredChristmasDestroyerItemManager {
                 "",
                 "${ChatColor.GOLD}스킬: ${ChatColor.of("#2a00ad")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}R${ChatColor.of("#2c07b6")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}e${ChatColor.of("#2e0ebf")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}f${ChatColor.of("#3115c8")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}l${ChatColor.of("#331cd1")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}e${ChatColor.of("#3523da")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}c${ChatColor.of("#382ae3")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}t${ChatColor.of("#3a31ec")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}i${ChatColor.of("#3c38f5")}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}o${ChatColor.of("#0000ff")}${ChatColor.BLUE}${ChatColor.BOLD}${ChatColor.ITALIC}${ChatColor.UNDERLINE}n${ChatColor.WHITE}${ChatColor.BOLD} ⚝ ${ChatColor.YELLOW}${ChatColor.BOLD}RIGHT CLICK",
                 "${ChatColor.RESET}${ChatColor.YELLOW}30 ${ChatColor.GRAY}경험치를 소모하여 ${ChatColor.GREEN}10초${ChatColor.GRAY}간 반사 쉴드를",
-                "${ChatColor.RESET}${ChatColor.GRAY}적용한다. 쉴드 상태에서 받은 피해를 ",
-                "${ChatColor.RESET}${ChatColor.GRAY}공격자에게 반사한다. 1회 반사 후 쉴드는 파괴된다.",
+                "${ChatColor.RESET}${ChatColor.GRAY}적용한다. 쉴드 상태에서 받은 피해를 공격자",
+                "${ChatColor.RESET}${ChatColor.GRAY}에게 반사한다. 1회 반사 후 쉴드는 파괴된다.",
                 "${ChatColor.GRAY}쿨다운: ${ChatColor.GREEN}15초",
                 ""
             )
